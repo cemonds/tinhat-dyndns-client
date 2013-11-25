80d22efa0d7585ebe0e80264ed3e8d646d41ebec
Christoph Emonds <info@christoph-emonds.de>
2013-11-25 19:41:26 +0100

    - call method based on parsed command

--- dyndns-client.py
+++ dyndns-client.py
@@ -17,11 +17,11 @@ parser_update.add_argument('--hostname', help='the hostname to update', required
 parser_update.add_argument('--ipv4', help='the ipv4 address', required=True)
 parser_update.add_argument('--ipv6', help='the ipv6 address', default='')
 
-parser_create = subparsers.add_parser('query', help='queries a hostname')
-parser_create.add_argument('--hostname', help='the hostname to query', required=True)
+parser_query = subparsers.add_parser('query', help='queries a hostname')
+parser_query.add_argument('--hostname', help='the hostname to query', required=True)
 
-parser_create = subparsers.add_parser('delete', help='deletes a hostname')
-parser_create.add_argument('--hostname', help='the hostname to query', required=True)
+parser_delete = subparsers.add_parser('delete', help='deletes a hostname')
+parser_delete.add_argument('--hostname', help='the hostname to query', required=True)
 
 args = parser.parse_args()
 
